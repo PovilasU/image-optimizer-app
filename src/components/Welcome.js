7b6ae8f13a9a50effee052e083a069919200c42a
povilasurbonas <povilas.urbonas85@gmail.com>
2020-03-22 12:11:47 +0100

    disabling prop types

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,6 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-export default function Welcome() {
-  return <p>React {this.props.name}</p>;
+export default function Welcome(props) {
+  return <p>React {props.name}</p>;
 }
