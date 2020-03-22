74270e30cb72d0dd974caff97573f6964e720388
povilasurbonas <povilas.urbonas85@gmail.com>
2020-03-22 14:33:34 +0100

    added rule to esling , added nav bar , added routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import Gallery from './Gallery/Gallery.js';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-export default function App(props) {
-  return (
-    <div>
-      <p>App {props.name}</p>
-      <Gallery></Gallery>
-    </div>
-  );
+import Home from './Home.js';
+import About from './About.js';
+import NavigationBar from './NavigationBar.js';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <NavigationBar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/home" component={Home} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
+// import Welcome from './components/Welcome.js';
 import App from './components/App.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 
-ReactDOM.render(
-  <App name="test app component."></App>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
